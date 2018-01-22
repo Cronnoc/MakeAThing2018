201a6527560502838f62b5b30655e50561a08b16
Caleb Dunwell <[caleb_j_d@live.com]>
2018-01-22 15:14:13 +1100

    Started On Game Menus

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+    int lives;
+    GameObject GameController;
+
+	// Use this for initialization
+	void Start () {
+
+        LoadLevel( PlayerPrefs.GetInt("Level"), PlayerPrefs.GetInt("Lives"));
+        GameController = GameObject.FindGameObjectWithTag("GameController");
+
+    }
+	
+    void LoadLevel(int Level, int Lives)
+    {
+        //pars the vars to game controller scpt 
+    }
+
+   public void EndLevel()
+    {
+        PlayerPrefs.SetInt("Level", (PlayerPrefs.GetInt("Level")+1));
+        PlayerPrefs.SetInt("Lives", lives);
+        SceneManager.LoadScene(PlayerPrefs.GetInt("Level"));
+    }
+}
