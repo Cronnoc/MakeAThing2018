ded5f5cd1ae79edc3847ba8151521fd0fa4b9f91
Connor Hasson <connor.h20@hotmail.com>
2018-01-22 13:54:44 +1000

    Player can move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float maxLives;
+    float lives;
+
+    [Header("Movement Options")]
+    public float movementSpeed;
+    public float swipeDeltaForJump;
+    public float jumpSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        lives = maxLives;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).position.x > Screen.width / 2)
+            {
+                rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime);
+            }
+            else if (Input.GetTouch(0).position.x < Screen.width / 2)
+            {
+                rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime);
+            }
+
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if (Input.GetTouch(i).deltaPosition.y > swipeDeltaForJump && IsGrounded())
+                {
+                    float additionalMovement = Input.GetTouch(i).deltaPosition.y - swipeDeltaForJump;
+                    float multiplier = 1.0f + (0.025f * additionalMovement);
+                    if (multiplier > 1.5f)
+                    {
+                        multiplier = 1.5f;
+                    }
+                    rb.AddExplosionForce(jumpSpeed * multiplier, transform.position, 100);
+                    break;
+                }
+            }
+        }
+
+
+        // Computer Testing code
+        if (Input.mousePosition.x > Screen.width / 2 && Input.GetMouseButton(0))
+        {
+            rb.AddForce(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        if (Input.mousePosition.x < Screen.width / 2 && Input.GetMouseButton(0))
+        {
+            rb.AddForce(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddExplosionForce(jumpSpeed, transform.position, 100);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.55f))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Die()
+    {
+        if (lives > 0)
+        {
+            lives--;
+            // respawn
+        }
+        else
+        {
+            //game over
+        }
+    }
+}
