c8c05ac5d90d5de2e996ae5485ce7b9c2d12b84f
Connor Hasson <connor.h20@hotmail.com>
2018-01-22 14:48:52 +1000

    Enemy can move

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject waypoint1, waypoint2;
+    int currentWaypoint;
+
+    public float movementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentWaypoint = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (currentWaypoint == 1)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.position - waypoint2.transform.position);
+            targetRotation = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
+            if (transform.rotation.y >= targetRotation.y + 0.1f || transform.rotation.y <= targetRotation.y - 0.1f)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 180 * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, waypoint1.transform.position, movementSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.position - waypoint1.transform.position);
+            targetRotation = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
+            if (transform.rotation.y >= targetRotation.y + 0.1f || transform.rotation.y <= targetRotation.y - 0.1f)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 180 * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, waypoint2.transform.position, movementSpeed * Time.deltaTime);
+            }
+        }
+
+        if (transform.position.x <= waypoint1.transform.position.x)
+        {
+            currentWaypoint = 2;
+        }
+        else if (transform.position.x >= waypoint2.transform.position.x)
+        {
+            currentWaypoint = 1;
+        }
+    }
+}
